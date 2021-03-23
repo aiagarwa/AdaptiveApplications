39774171bca5c173ee97052ef90bf4670e87486b
xurui1995 <ruixutcd@gmail.com>
2021-03-23 19:59:31 +0000

    add database connection

--- user.py
+++ user.py
@@ -1,4 +1,10 @@
-class User:
+from extension import db
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(20), unique=True)
+
     def __init__(self, name='John', age=25):
 
         self.name = name
